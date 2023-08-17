db9b9c39e8695c2baa775177561dfd9db6d00061
llgomes <luis.gomes@foton.la>
2023-08-17 14:52:06 -0300

    Criando a Classe para o Jogo de Adivinhação.

--- src/JogoAdivinhacao.java
+++ src/JogoAdivinhacao.java
@@ -1,35 +1,31 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
+
 public class JogoAdivinhacao {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-
-        int numeroAleatorio = random.nextInt(101); // Gera um número aleatório entre 0 e 100
-        int tentativas = 5;
+        Scanner leitor = new Scanner(System.in);
+        int numeroAleatorio = new Random().nextInt(100); // gera um número aleatório entre 0 e 100
+        int tentativas = 0;
 
         System.out.println("Bem-vindo ao Jogo de Adivinhação!");
         System.out.println("Tente adivinhar o número entre 0 e 100.");
 
-        for (int i = 1; i <= tentativas; i++) {
-            System.out.print("Tentativa " + i + ": Digite um número: ");
-            int numeroDigitado = scanner.nextInt();
+        while (tentativas < 5) {
+            System.out.print("Digite um número entre 0 e 100: ");
+            int numeroDigitado = leitor.nextInt();
+            tentativas++;
 
             if (numeroDigitado == numeroAleatorio) {
-                System.out.println("Parabéns! Você acertou o número.");
-                break; // Encerra o jogo
+                System.out.println("Parabéns, você acertou o número em " + tentativas + " tentativas!");
+                break; // Termina o jogo
             } else if (numeroDigitado < numeroAleatorio) {
-                System.out.println("O número gerado é maior.");
+                System.out.println("O número digitado é menor que o número gerado.");
             } else {
-                System.out.println("O número gerado é menor.");
-            }
-
-            if (i < tentativas) {
-                System.out.println("Tente novamente.");
-            } else {
-                System.out.println("Você atingiu o limite de tentativas. O número era: " + numeroAleatorio);
+                System.out.println("O número digitado é maior que o número gerado.");
             }
         }
-
+        if (tentativas == 5) {
+            System.out.println("Não foi destas vez. O Número gerado era: " + numeroAleatorio);
+        }
     }
 }
