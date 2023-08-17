967a9bdaa30334ae193251cad779c8123e68534a
llgomes <luis.gomes@foton.la>
2023-08-17 10:18:31 -0300

    Testando switch.

--- src/Condicional.java
+++ src/Condicional.java
@@ -16,5 +16,37 @@ public class Condicional {
         } else {
             System.out.println("Deve pagar a locação");
         }
+
+        int dia = 5;
+        String nomeDia;
+
+        switch (dia) {
+            case 1:
+                nomeDia = "domingo";
+                break;
+            case 2:
+                nomeDia = "segunda-feira";
+                break;
+            case 3:
+                nomeDia = "terça-feira";
+                break;
+            case 4:
+                nomeDia = "quarta-feira";
+                break;
+            case 5:
+                nomeDia = "quinta-feira";
+                break;
+            case 6:
+                nomeDia = "sexta-feira";
+                break;
+            case 7:
+                nomeDia = "sábado";
+                break;
+            default:
+                nomeDia = "Dia inválido";
+                break;
+        }
+
+        System.out.println("O dia " + dia + " é " + nomeDia);
     }
 }
